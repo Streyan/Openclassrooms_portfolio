35dde90d0b9e9e8ca3b253da628b749a927620ff
Streyan <leplongeon.celine@gmail.com>
2025-05-27 15:34:07 +0200

    fix responsive

--- src/components/presentation.tsx
+++ src/components/presentation.tsx
@@ -1,7 +1,10 @@
 function Presentation() {
   return (
     <div className="flex-column">
-      <p className="headingXL black">Bienvenue ! Je suis Céline Leplongeon.</p>
+      <p className="headingXL black">
+        Bienvenue ! Je suis
+        <span className="headingXL black underline"> Céline Leplongeon</span>.
+      </p>
       <p className="text black">
         Basée à Montpellier, France, je suis une développeuse front-end.
       </p>
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -3,7 +3,7 @@ import skills from "../../skills.json";
 
 function Skills() {
   return (
-    <div className="flex-row skills">
+    <div className="skills">
       {skills.map((skill) => (
         <Skill xp={skill.xp} title={skill.name} />
       ))}
